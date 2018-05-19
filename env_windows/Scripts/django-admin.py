0ae69a4aa95b0a7d1d2bd990000b289564d5ec81
kuma0923 <railgun86923@gmail.com>
2018-05-19 17:41:39 +0800

    'owo'

---
+++ env_windows/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\pockm\desktop\107-team09-master\env_windows\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
