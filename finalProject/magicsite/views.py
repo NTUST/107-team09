cdcf798a0f2964af8bb17f2d320862d9dfbdf900
FormyHarmony <36830367+FormyHarmony@users.noreply.github.com>
2018-05-31 14:00:11 +0800

    0531

--- finalProject/magicsite/views.py
+++ finalProject/magicsite/views.py
@@ -10,4 +10,20 @@ def index(request):
 	return render_to_response("index.html",locals())
 	
 def home(request):
-    return render(request, 'magicsite/home.html')
+    return render(request, 'home.html')
+    
+def index(request):
+	question_sets=Question_Set.objects.all()
+	return render_to_response("result.html",locals())
+	
+
+# dragon
+def wand(request):
+	magic_Wand=Magic_Wand.objects.all()
+	return render_to_response("wand.html",locals())
+
+# dragon
+def result(request):
+	magic_Wand=Magic_Wand.objects.all()
+	mahou_Shoujo=Mahou_Shoujo.objects.all()
+	return render_to_response("result.html",locals())
