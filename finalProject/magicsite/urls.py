dc71c1f81aa2886b3f64c1591fe03fe2de0a7894
FormyHarmony <36830367+FormyHarmony@users.noreply.github.com>
2018-06-04 11:14:59 +0800

    11:14

--- finalProject/magicsite/urls.py
+++ finalProject/magicsite/urls.py
@@ -3,5 +3,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-	url(r'^$',views.index, name='index'),
+	url(r'^$',views.home, name='home'),
+	url(r'^result/$',views.result, name='result'),
+	url(r'^wand/$',views.wand, name='wand'),
+	url(r'^menu/$',views.menu, name='menu'),
+	url(r'^question/$',views.index, name='question'),
 ]
--- finalProject/magicsite/views.py
+++ finalProject/magicsite/views.py
@@ -10,7 +10,7 @@ def index(request):
 	return render_to_response("index.html",locals())
 	
 def home(request):
-    return render(request, 'home.html')
+    return render_to_response("home.html",locals())
 	
 
 # dragon
