493308ed9c7953ac0362cc3acb502b279e32a94c
a9630121a9630121 <a9630121a9630121@gmail.com>
2018-06-05 18:15:27 +0800

    0605

--- finalProject/magicsite/views.py
+++ finalProject/magicsite/views.py
@@ -12,19 +12,14 @@ def index(request):
 def home(request):
     return render_to_response("home.html",locals())
     
-
-	
-# dragon
 def menu(request):
 	question_sets=Question_Set.objects.all()
 	return render_to_response("menu.html",locals())
 
-# dragon
 def wand(request):
 	magic_Wand=Magic_Wand.objects.all()
 	return render_to_response("wand.html",locals())
 
-# dragon
 def result(request):
 	magic_Wand=Magic_Wand.objects.all()
 	mahou_Shoujo=Mahou_Shoujo.objects.all()
