c5cdb11d19ffb1ad057cadd56aad7e716fe6c724
Jellyfish <turtle20953809@gmail.com>
2018-06-05 20:59:10 +0800

    owo

--- finalProject/magicsite/urls.py
+++ finalProject/magicsite/urls.py
@@ -4,12 +4,8 @@ from django.contrib.auth.views import login, logout
 from . import views
 
 urlpatterns=[
-  	url(r'^$',views.index, name='index'),
-	url(r'^login/$',views.login,name='login'),
-	url(r'^logout/$',views.logout,name='logout'),
-	url(r'^register/$',views.register,name='register'),
 	url(r'^result/$',views.result, name='result'),
 	url(r'^wand/$',views.wand, name='wand'),
-  url(r'^menu/$',views.menu, name='menu'),
+	url(r'^menu/$',views.menu, name='menu'),
 	url(r'^question/$',views.question, name='question'),
 ]
--- finalProject/magicsite/views.py
+++ finalProject/magicsite/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse,HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 from django.contrib import auth
@@ -10,30 +10,23 @@ from .models import *
 def index(request):
 	return render(request, 'index.html')
 
-def home(request):
-  return render(request, 'home.html')
-    
-def menu(request):
-	question_sets=Question_Set.objects.all()
-  return render(request, 'menu.html')
-
-
-def register(request):
-	if request.method == 'POST':
-		username = request.POST.get('username')
-		password = request.POST.get('password')
-		repassword = request.POST.get('repassword')
-		if password == repassword:
-			check_user = User.objects.filter(username=username)
-			if len(check_user) == 0:
-				user = User.objects.create_user(username, '', password)
-				user.backend = 'django.contrib.auth.backends.ModelBackend'
-				auth.login(request, user)
-				return redirect('/magicsite/')
-			else: return HttpResponse("<script>alert('使用者名稱重複');window.location = '/magicsite/register/';</script>")
-		else: return HttpResponse("<script>alert('密碼與確認密碼不同');window.location = '/magicsite/register/';</script>")
-		form = UserCreationForm(request.POST)
-	return render(request,'register.html')
+def regist(request):
+	if not request.user.is_authenticated(): 
+		if request.method == 'POST':
+			username = request.POST.get('username')
+			password = request.POST.get('password')
+			repassword = request.POST.get('repassword')
+			if password == repassword:
+				check_user = User.objects.filter(username=username)
+				if len(check_user) == 0:
+					user = User.objects.create_user(username, '', password)
+					user.backend = 'django.contrib.auth.backends.ModelBackend'
+					auth.login(request, user)
+					return redirect('/magicsite/menu/')
+				else: return HttpResponse("<script>alert('使用者名稱重複');window.location = '/magicsite/register/';</script>")
+			else: return HttpResponse("<script>alert('密碼與確認密碼不同');window.location = '/magicsite/register/';</script>")
+		return render(request,'register.html')
+	else: return redirect('/magicsite/menu/')
 
 def login(request):
 	if not request.user.is_authenticated(): 
@@ -42,19 +35,25 @@ def login(request):
 		user = auth.authenticate(username=username, password=password)
 		if user is not None:
 			auth.login(request, user)
-			return redirect('/magicsite/')
-		else: return render(request, 'login.html', {})
-	else: return redirect('/magicsite/')
+			return redirect('/magicsite/menu/')
+		else: return render(request, 'login.html')
+	else: return redirect('/magicsite/menu/')
 
 def logout(request):
 	auth.logout(request)
-	return redirect('/magicsite/')
+	return redirect('/login/')
 
+@login_required
+def menu(request):
+	question_sets = Question_Set.objects.all()
+	return render(request, 'menu.html', {'question_sets': question_sets})
 
+@login_required
 def question(request):
 	question = Question.objects.all()
 	return render(request, "question.html", {'question': question})
 
+@login_required
 def post(request):
 	if request.POST.get('max') > 0:
 		if request.POST.get('max') == 1:
@@ -69,10 +68,12 @@ def post(request):
 	#else:
 		#return render(request, "", {: })
 
+@login_required
 def wand(request):
 	magic_Wand = Magic_Wand.objects.all()
 	return render(request, "wand.html", {'magic_Wand': magic_Wand})
 
+@login_required
 def result(request):
 	magic_Wand = Magic_Wand.objects.all()
 	mahou_Shoujo = Mahou_Shoujo.objects.all()
