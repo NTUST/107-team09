997005082666d214eaff22c010e0ca8974f71886
kuma0923 <railgun86923@gmail.com>
2018-06-09 10:31:53 +0800

    admin

--- finalProject/magicsite/admin.py
+++ finalProject/magicsite/admin.py
@@ -5,6 +5,11 @@ from .models import *
 
 class QuestionInline(admin.TabularInline):
 	model = Question
+class OptionInline(admin.TabularInline):
+	model = Option
+class Magic_WandInline(admin.TabularInline):
+	model = Magic_Wand
+
 
 class QuestionSetAdmin(admin.ModelAdmin):
 	fieldsets = [
@@ -14,9 +19,27 @@ class QuestionSetAdmin(admin.ModelAdmin):
 	list_display = ('id', 'title')
 	list_filter = ['title']
 	search_fields = ['title']
+class QuestionAdmin(admin.ModelAdmin):
+	fieldsets = [
+		(None, {'fields': ['question_set', 'text']}),
+	]
+	inlines = [OptionInline]
+	list_display = ('id', 'question_set')
+	list_filter = ['question_set']
+	search_fields = ['question_set']
+class MahouShoujoAdmin(admin.ModelAdmin):
+	fieldsets = [
+		(None, {'fields': ['name', 'desc']}),
+	]
+	inlines = [Magic_WandInline]
+	list_display = ('id', 'name')
+	list_filter = ['name']
+	search_fields = ['name']
 
 admin.site.register(Question_Set, QuestionSetAdmin)
-admin.site.register(Question)
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Option)
-admin.site.register(Mahou_Shoujo)
+admin.site.register(Mahou_Shoujo, MahouShoujoAdmin)
 admin.site.register(Magic_Wand)
+admin.site.register(User_Mahou_Shoujo)
+admin.site.register(User_Question_Set)
