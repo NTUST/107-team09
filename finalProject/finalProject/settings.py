f3a5c51a0074ca5dce478da2c5472f0a7520ea64
Jellyfish <turtle20953809@gmail.com>
2018-06-10 22:45:48 +0800

    owo

--- finalProject/finalProject/settings.py
+++ finalProject/finalProject/settings.py
@@ -108,7 +108,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
@@ -121,7 +120,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
